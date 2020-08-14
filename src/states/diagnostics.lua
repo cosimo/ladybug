36e0009858789c55afb54ee9a1319acb7cee1d2b
Cosimo Streppone <cosimo@kahoot.com>
2020-08-14 17:09:08 +0200

    Fixed diagnostics text position

--- src/states/diagnostics.lua
+++ src/states/diagnostics.lua
@@ -1,8 +1,8 @@
 local st = {}
 
 function st.init()
-  st.waited = 0.0
-  st.next_state = states.attractmode
+    st.waited = 0.0
+    st.next_state = states.attractmode
 end
 
 function st.enter(prev)
@@ -15,22 +15,22 @@ function st.resume()
 end
 
 function st.update(self, dt)
-  self.waited = st.waited + dt
-  if self.waited > 4 then
-    gs.switch(self.next_state)
-  end
+    self.waited = st.waited + dt
+    if self.waited > 2 then
+        gs.switch(self.next_state)
+    end
 end
 
 function st.draw()
-  memory_check()
+    memory_check()
 end
 
 function memory_check()
-  push:start()
-  love.graphics.setColor(0xfc/255, 0xb2/255, 0xf2/255)
-  love.graphics.print("ROM OK", 60, 40)
-  love.graphics.print("RAM OK", 60, 64)
-  push:finish()
+    push:start()
+    love.graphics.setColor(0xfc/255, 0xb2/255, 0xf2/255)
+    love.graphics.print("ROM OK", 72, 90)
+    love.graphics.print("RAM OK", 72, 114)
+    push:finish()
 end
 
 return st
