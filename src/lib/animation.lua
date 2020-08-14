0f105ccbaa86b7634eac7cac22332148d024922b
Cosimo Streppone <cosimo@kahoot.com>
2020-08-14 17:08:04 +0200

    Improved animation lib argument names

--- src/lib/animation.lua
+++ src/lib/animation.lua
@@ -2,34 +2,37 @@
 
 local ezanim = {}
 
-function ezanim.newtemplate(png,w,s,b,l)
+function ezanim.newtemplate(png, width, speed, border, loop)
   local t = {}
   local cycle_frames_backwards = true
 
-  t.w = w
-  t.s = s
-  t.b = b or 0
-  if l == nil then
-    t.l = true
+  t.width = width
+  t.speed = speed
+  t.border = border or 0
+
+  if loop == nil then
+    t.loop = true
   else
-    t.l = l
+    t.loop = loop
   end
+
   t.img = love.graphics.newImage("assets/" .. png .. ".png")
-  t.h = t.img:getHeight()
-  t.frames = t.img:getWidth()/(w+t.b)
+  t.height = t.img:getHeight()
+  t.frames = t.img:getWidth()/(width + t.border)
   t.quads = {}
+
   local offset = 0
-  for i=0, t.frames - 1 do
-    quad = love.graphics.newQuad(i * t.w + offset, 0 , t.w, t.h, t.img:getWidth(), t.img:getHeight())
+  for i = 0, t.frames - 1 do
+    quad = love.graphics.newQuad(i * t.width + offset, 0 , t.width, t.height, t.img:getWidth(), t.img:getHeight())
     table.insert(t.quads, quad)
-    offset = offset + t.b
+    offset = offset + t.border
   end
 
   if cycle_frames_backwards then
-      for i=t.frames - 2, 1, -1 do
-        quad = love.graphics.newQuad(i * t.w + offset, 0 , t.w, t.h, t.img:getWidth(), t.img:getHeight())
+      for i = t.frames - 2, 1, -1 do
+        quad = love.graphics.newQuad(i * t.width + offset, 0 , t.width, t.height, t.img:getWidth(), t.img:getHeight())
         table.insert(t.quads, quad)
-        offset = offset - t.b
+        offset = offset - t.border
         t.frames = t.frames + 1
       end
   end
@@ -41,25 +44,25 @@ end
 function ezanim.newanim(temp)
   local a = {}
   a.temp = temp
-  a.f = 1
+  a.frame = 1
   a.time = 0
   return a
 end
 
 function ezanim.animupdate(a,dt)
   a.time = a.time + dt
-  if a.temp.s ~= 0 then
-    if a.time >= a.temp.s then
-      framesmissed = math.floor(a.time / a.temp.s)
-      a.f = a.f + framesmissed
-      a.time = a.time - framesmissed * a.temp.s
+  if a.temp.speed ~= 0 then
+    if a.time >= a.temp.speed then
+      framesmissed = math.floor(a.time / a.temp.speed)
+      a.frame = a.frame + framesmissed
+      a.time = a.time - framesmissed * a.temp.speed
       
     end
-    while a.f >= a.temp.frames + 1 do
-      if a.temp.l then
-        a.f = a.f - a.temp.frames
+    while a.frame >= a.temp.frames + 1 do
+      if a.temp.loop then
+        a.frame = a.frame - a.temp.frames
       else
-        a.f = a.temp.frames
+        a.frame = a.temp.frames
       end
     end 
   else
@@ -76,7 +79,7 @@ function ezanim.animdraw(a,x,y,r,sx,sy,ox,oy,kx,ky)
   oy = oy or 0
   kx = kx or 0
   ky = ky or 0
-  quad = a.temp.quads[a.f]
+  quad = a.temp.quads[a.frame]
   if a.temp.type == "4color" then
     for i=1,4 do
       love.graphics.setColor(colors[i])
@@ -88,8 +91,8 @@ function ezanim.animdraw(a,x,y,r,sx,sy,ox,oy,kx,ky)
 end
 
 function ezanim.resetanim(a)
-  a.f=1
-  a.time=0
+  a.frame = 1
+  a.time = 0
 end
 
 return ezanim
