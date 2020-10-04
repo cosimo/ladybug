bbcaa04f1f462639f862d989b53ab4fac407f68f
Cosimo Streppone <cosimo@streppone.it>
2020-10-04 20:50:18 +0200

    Added special dollar coin icon/sprite object

---
+++ src/obj/coin.lua
@@ -0,0 +1,26 @@
+-- Dollar coin static sprite code
+
+local obj = {
+    layer = -4,
+    uplayer = 4,
+    initialized = false,
+    delete = false,
+    image = love.graphics.newImage("assets/obj/coin.png"),
+    x = 8,
+    y = 8,
+    speed = 0,
+    angle = 0
+}
+
+function obj.init()
+    obj.initialized = true
+end
+
+function obj.update(dt)
+end
+
+function obj.draw()
+    love.graphics.draw(obj.image, obj.x, obj.y, obj.angle)
+end
+
+return obj
