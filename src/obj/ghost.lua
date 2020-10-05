159cfd7d46fa5dc5241459ee79920ea9a2453f8b
Cosimo Streppone <cosimo@streppone.it>
2020-10-05 21:49:46 +0200

    Added ladybug "ghost" sprite/animation for when 🐞 dies

---
+++ src/obj/ghost.lua
@@ -0,0 +1,30 @@
+-- Ladybug death sprite/animation code
+
+local obj = {
+    layer = -3,
+    uplayer = 3,
+    anim = animation.newanim(
+        animation.newtemplate("obj/ghost", 16, 0.125,
+                0, animation.loop.NONE)
+    ),
+    initialized = false,
+    delete = false,
+    speed = 1,
+    x = 24,
+    y = 180,
+    angle = 0
+}
+
+function obj.init()
+    obj.initialized = true
+end
+
+function obj.update(dt)
+    animation.animupdate(obj.anim, dt)
+end
+
+function obj.draw()
+    animation.animdraw(obj.anim, obj.x, obj.y, obj.angle, 1, 1, 8, 8)
+end
+
+return obj
